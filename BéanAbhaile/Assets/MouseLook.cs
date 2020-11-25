5e4125e3617c4a0d081ed00fd25138f231313e6f
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2020-11-25 00:59:04 +0000

    pushthing

---
+++ BéanAbhaile/Assets/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    private float _mouseSensitivity = 100;
+    public Transform playerBody;
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * _mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * _mouseSensitivity;
+
+        xRotation -= mouseY;
+
+        transform.localRotation = Quaternion.Euler(xRotation,0,0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
