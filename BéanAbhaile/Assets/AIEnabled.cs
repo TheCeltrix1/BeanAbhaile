7f6770dd861b1dcda6df239bae92fcfe25f41992
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2020-12-15 23:47:59 +0000

    Ai turning on

---
+++ BéanAbhaile/Assets/AIEnabled.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIEnabled : MonoBehaviour
+{
+    public GameObject banshee;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.GetComponent<Player>()) banshee.GetComponent<Banshee>().AIEnable();
+    }
+}
