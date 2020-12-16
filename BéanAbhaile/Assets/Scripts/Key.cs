1cf15356680b0440c517242b7a535a4ddcf6a72a
Gomystalka <48233064+Gomystalka@users.noreply.github.com>
2020-12-16 01:10:41 +0000

    Revert "Revert "Revert "Revert "Added doors and Kagi chan""""
    This reverts commit 594bf1e0438d547b15a9f08c64befc56aba305e8.

---
+++ BéanAbhaile/Assets/Scripts/Key.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : Interactable
+{
+    public bool PickedUp { get; private set; }
+
+    public override void OnUse()
+    {
+        if (PickedUp) return;
+        PickedUp = true;
+        if(meshRenderer)
+            meshRenderer.enabled = false;
+    }
+}
